f2c9104f852d213c34e176fcff47a588db44517e
Anton Struyk <astruyk@gmail.com>
2014-08-01 11:56:13 -0400

    Add batch file to generate PBO (untested)

---
+++ scripts/Generate_PBO.bat
@@ -0,0 +1,15 @@
+@echo off
+IF "%1"=="" GOTO no_args
+echo Generating PBO for mission '%1'
+SET MISSION_ISLAND=%1
+goto done
+:no_args
+echo No island specified, assuming Altis.
+SET MISSION_ISLAND=Altis
+:done
+@echo on
+set BASE_FILE_NAME=JTF2_Test_Private_Mission
+..\lib\PBOManager_v_1_4_beta\PBOConsole.exe -pack ..\mission ..\bin\%BASE_FILE_NAME%.%MISSION_ISLAND%.pbo
+if exist ..\bin\%BASE_FILE_NAME%.%MISSION_ISLAND%.pbo.bak del ..\bin\%BASE_FILE_NAME%.%MISSION_ISLAND%.pbo.bak
+@echo off
+echo Done generating PBO for '%MISSION_ISLAND%'
