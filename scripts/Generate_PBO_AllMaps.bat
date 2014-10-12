89c5baee728195967f39169f9cd55fa59d18846a
Anton <Anton@.(none)>
2014-10-12 19:28:51 -0400

    Add script to generate all PBO's

---
+++ scripts/Generate_PBO_AllMaps.bat
@@ -0,0 +1,7 @@
+call .\Generate_PBO.bat Altis
+call .\Generate_PBO.bat Chernarus
+call .\Generate_PBO.bat Takistan
+call .\Generate_PBO.bat Woodland_ACR
+call .\Generate_PBO.bat Utes
+call .\Generate_PBO.bat Zagarabad
+call .\Generate_PBO.bat Bystrica
