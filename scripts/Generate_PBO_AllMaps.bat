63c8cc35f9ee7a293b4c23610a1c0d2c7b71194f
anton <astruyk@gmail.com>
2014-11-14 13:10:44 -0500

    Cleanup the map .bat file a bit. Add Sahrani. Remove Utes.

--- scripts/Generate_PBO_AllMaps.bat
+++ scripts/Generate_PBO_AllMaps.bat
@@ -1,9 +1,15 @@
+REM Vanilla Maps
 call .\Generate_PBO.bat Altis
+call .\Generate_PBO.bat Stratis
+call .\Generate_PBO.bat VR
+
+REM All In Arma TP
 call .\Generate_PBO.bat Chernarus
 call .\Generate_PBO.bat Takistan
 call .\Generate_PBO.bat Woodland_ACR
-call .\Generate_PBO.bat Utes
 call .\Generate_PBO.bat Zargabad
 call .\Generate_PBO.bat Bystrica
+call .\Generate_PBO.bat Sahrani
+
+REM Wake Island
 call .\Generate_PBO.bat Wake
-call .\Generate_PBO.bat VR
