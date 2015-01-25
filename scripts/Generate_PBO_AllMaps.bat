4e7ba1ea5ad4893979085df88c549ac12eec92f5
anton <astruyk@gmail.com>
2015-01-25 15:23:19 -0500

    Add 'Woodland_ACR' since apparently in 1.38 that's the name Bystrica goes by.

--- scripts/Generate_PBO_AllMaps.bat
+++ scripts/Generate_PBO_AllMaps.bat
@@ -11,6 +11,9 @@ call .\Generate_PBO.bat Bystrica
 call .\Generate_PBO.bat Sara
 call .\Generate_PBO.bat Porto
 call .\Generate_PBO.bat Bystrica
+REM Sometimes (apparently) Bystrica is known by this name.
+call .\Generate_PBO.bat Woodland_ACR
+
 
 REM Wake Island
 call .\Generate_PBO.bat Wake
